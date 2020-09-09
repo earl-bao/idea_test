9e6592d27b8c6e4205609cfec7c248f4b85f5e79
earl_Bao <429152352@qq.com>
2020-09-09 15:45:38 +0800

    测试01

---
+++ src/test.java
@@ -0,0 +1,12 @@
+/**
+ * @Package: PACKAGE_NAME
+ * @Description： test01
+ * @Author: earl_Bao
+ * @Date: 2020/9/9 15:43
+ * @Version: 1.0
+ */
+public class    test {
+    public static void main(String[] args) {
+        System.out.println( "hello" );
+    }
+}
