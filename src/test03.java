512682305e1832534aec58701d4a6072b388d61d
earl_Bao <429152352@qq.com>
2020-09-09 16:18:04 +0800

    测试04

---
+++ src/test03.java
@@ -0,0 +1,12 @@
+/**
+ * @Package: PACKAGE_NAME
+ * @Description： test
+ * @Author: earl_Bao
+ * @Date: 2020/9/9 16:17
+ * @Version: 1.0
+ */
+public class test03 {
+    public static void main(String[] args) {
+        System.out.println( "bbb" );
+    }
+}
