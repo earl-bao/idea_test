3c3236a3bd97a8978dca46c2617dee141f609a1c
earl_Bao <429152352@qq.com>
2020-09-09 16:08:50 +0800

    测试03

---
+++ src/test02.java
@@ -0,0 +1,12 @@
+/**
+ * @Package: PACKAGE_NAME
+ * @Description： test
+ * @Author: earl_Bao
+ * @Date: 2020/9/9 16:07
+ * @Version: 1.0
+ */
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println( "aaa" );
+    }
+}
