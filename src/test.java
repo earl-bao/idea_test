878a27b1d93fbf15eca2f4c1b509423f92926b9d
earl_Bao <429152352@qq.com>
2020-09-09 16:04:15 +0800

    测试02

--- src/test.java
+++ src/test.java
@@ -8,5 +8,21 @@
 public class    test {
     public static void main(String[] args) {
         System.out.println( "hello" );
+        System.out.println( "hello2" );
+        int a = num(5,1);
+        System.out.println( a );
+
+    }
+
+    /**
+    * @Description:
+    * @Param: [a, b]
+    * @returnType: int
+    * @Author: earl_Bao
+    * @Date: 2020/9/9
+    * @Version: 1.0
+    */
+    public static int num(int a,int b){
+        return a+b;
     }
 }
